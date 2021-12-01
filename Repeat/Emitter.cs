93b10f1dcd180bd6bc1e270ff0ce324790696de0
Bert-10 <misha.rogrant@mail.ru>
2021-12-01 22:21:52 +0800

    Созданы точки гравитации

--- Repeat/Emitter.cs
+++ Repeat/Emitter.cs
@@ -14,7 +14,9 @@ namespace Repeat
         public int MousePositionY;
 
         public float GravitationX = 0;
-        public float GravitationY = 1;
+        public float GravitationY = 0;
+
+        public List<Point> gravityPoints = new List<Point>();
 
         public void UpdateState()
         {
@@ -36,6 +38,17 @@ namespace Repeat
                 }
                 else
                 {
+                    foreach (var point in gravityPoints)
+                    {
+                        float gX = point.X - particle.X;
+                        float gY = point.Y - particle.Y;
+                        float r2 = (float)Math.Max(100, gX * gX + gY * gY);
+                        float M = 100;
+
+                        particle.SpeedX += (gX) * M / r2;
+                        particle.SpeedY += (gY) * M / r2;
+                    }
+
                     particle.SpeedX += GravitationX;
                     particle.SpeedY += GravitationY;
                     // так как теперь мы храним вектор скорости в явном виде и его не надо пересчитывать
@@ -76,6 +89,17 @@ namespace Repeat
             foreach (var particle in particles)
             {
                 particle.Draw(g);
+            }
+
+            foreach (var point in gravityPoints)
+            {
+                g.FillEllipse(
+                    new SolidBrush(Color.Red),
+                    point.X - 5,
+                    point.Y - 5,
+                    10,
+                    10
+                );
             }
         }
     }
