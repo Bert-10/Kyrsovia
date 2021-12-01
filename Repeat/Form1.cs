ba828b1e87d2a97bb9e9e0944634c81e69831b81
Bert-10 <misha.rogrant@mail.ru>
2021-12-01 21:59:32 +0800

    Частицы летять из центра

--- Repeat/Form1.cs
+++ Repeat/Form1.cs
@@ -30,15 +30,52 @@ namespace Repeat
             }
         }
 
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1; // уменьшаю здоровье
+                                    // если здоровье кончилось
+                if (particle.Life < 0)
+                {
+                    // восстанавливаю здоровье
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    // перемещаю частицу в центр
+                    particle.X = picDisplay.Image.Width / 2;
+                    particle.Y = picDisplay.Image.Height / 2;
+
+                    particle.Direction = Particle.rand.Next(360);
+                    particle.Speed = 1 + Particle.rand.Next(10);
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    // а это наш старый код
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+        }
+
+        // функция рендеринга
+        private void Render(Graphics g)
+        {
+            // утащили сюда отрисовку частиц
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
+            UpdateState(); // каждый тик обновляем систему
+
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
                 g.Clear(Color.White);
-                foreach (var particle in particles)
-                {
-                    particle.Draw(g);
-                }
+                Render(g); // рендерим систему
             }
 
             picDisplay.Invalidate();
--- Repeat/Particle.cs
+++ Repeat/Particle.cs
@@ -14,15 +14,18 @@ namespace Repeat
         public float Y; // Y координата положения частицы в пространстве
         public float Direction; // направление движения
         public float Speed; // скорость перемещения
+        public float Life;
+
         public static Random rand = new Random();
+
 
         // конструктор по умолчанию будет создавать кастомную частицу
         public Particle()
         {
-            // я не трогаю координаты X, Y потому что хочу, чтобы все частицы возникали из одного места
             Direction = rand.Next(360);
             Speed = 1 + rand.Next(10);
             Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100); // Добавили исходный запас здоровья от 20 до 120
         }
         public void Draw(Graphics g)
         {
