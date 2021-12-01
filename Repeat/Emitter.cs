4bf5a11bffbb0d6b29809225f102fa4575182c0a
Bert-10 <misha.rogrant@mail.ru>
2021-12-01 22:28:31 +0800

    Создана точка антигравитации

--- Repeat/Emitter.cs
+++ Repeat/Emitter.cs
@@ -16,7 +16,7 @@ namespace Repeat
         public float GravitationX = 0;
         public float GravitationY = 0;
 
-        public List<Point> gravityPoints = new List<Point>();
+        public List<IImpactPoint> impactPoints = new List<IImpactPoint>();
 
         public void UpdateState()
         {
@@ -38,15 +38,9 @@ namespace Repeat
                 }
                 else
                 {
-                    foreach (var point in gravityPoints)
+                    foreach (var point in impactPoints)
                     {
-                        float gX = point.X - particle.X;
-                        float gY = point.Y - particle.Y;
-                        float r2 = (float)Math.Max(100, gX * gX + gY * gY);
-                        float M = 100;
-
-                        particle.SpeedX += (gX) * M / r2;
-                        particle.SpeedY += (gY) * M / r2;
+                        point.ImpactParticle(particle);
                     }
 
                     particle.SpeedX += GravitationX;
@@ -91,16 +85,13 @@ namespace Repeat
                 particle.Draw(g);
             }
 
-            foreach (var point in gravityPoints)
-            {
-                g.FillEllipse(
-                    new SolidBrush(Color.Red),
-                    point.X - 5,
-                    point.Y - 5,
-                    10,
-                    10
-                );
+            foreach (var point in impactPoints) 
+            {              
+                point.Render(g); 
             }
         }
+
     }
+
+    
 }
